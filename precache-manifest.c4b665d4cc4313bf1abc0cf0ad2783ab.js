a11d979dfe7e6213a230e90d5d75f206449722f3
BadrMohammed <56343558+BadrMohammed@users.noreply.github.com>
2019-11-11 09:46:45 +0200

    Updates

---
+++ precache-manifest.c4b665d4cc4313bf1abc0cf0ad2783ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a73db17cefe2af4b8de49f1eb234403",
+    "url": "/BadrMohammed/Simple_Youtube/index.html"
+  },
+  {
+    "revision": "5df71b861656558a3e32",
+    "url": "/BadrMohammed/Simple_Youtube/static/js/2.103521c7.chunk.js"
+  },
+  {
+    "revision": "c73777e452131a28e030",
+    "url": "/BadrMohammed/Simple_Youtube/static/js/main.4a55f683.chunk.js"
+  },
+  {
+    "revision": "1839081475f3723828ee",
+    "url": "/BadrMohammed/Simple_Youtube/static/js/runtime-main.f2c2d94a.js"
+  }
+]);
